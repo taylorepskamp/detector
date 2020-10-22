430a5983356df7d0541546c5f64b3a8e0763a8c9
Taylor Epskamp <taylorepskamp@gmail.com>
2020-10-22 15:51:42 -0700

    Updates

---
+++ precache-manifest.cb1a01e2eb9306d0b545bc197ea77982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e0f4d2b3c75591c9e312494697ffbf",
+    "url": "/detector/index.html"
+  },
+  {
+    "revision": "a23df19986f67fb94e35",
+    "url": "/detector/static/css/main.60257792.chunk.css"
+  },
+  {
+    "revision": "940bdcb5a8b39a531ba2",
+    "url": "/detector/static/js/2.e2113bcf.chunk.js"
+  },
+  {
+    "revision": "a7605411de1c8a0455359a663b8c9479",
+    "url": "/detector/static/js/2.e2113bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23df19986f67fb94e35",
+    "url": "/detector/static/js/main.d6ea0dbf.chunk.js"
+  },
+  {
+    "revision": "a33e914078ed62e0f9e8",
+    "url": "/detector/static/js/runtime-main.94f05227.js"
+  }
+]);
